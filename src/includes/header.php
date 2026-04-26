82db934fe697d0fbdb8505a554502589ab0f232b
Ergi <ergimandija1@gmail.com>
2026-04-26 20:14:27 +0200

    add relative paths

--- src/includes/header.php
+++ src/includes/header.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FTW-Connect</title>
 
-    <link rel="stylesheet" href="/css/header.css">
-    <link rel="stylesheet" href="/css/footer.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 </head>
